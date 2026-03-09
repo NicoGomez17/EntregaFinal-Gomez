399521171e87de24490a5083304e18c8f6b52622
Nicolas Gomez <nicogomez170401@gmail.com>
2026-03-09 19:19:34 -0300

    Modificando carpetas, modelos y admin

---
+++ src/futbol/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    fundacion = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name = "Equipo"
+        verbose_name_plural = "Equipos"
+
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    posicion = models.CharField(max_length=50)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name = "Jugador"
+        verbose_name_plural = "Jugadores"
+
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+    class Meta:
+        verbose_name = "Noticia"
+        verbose_name_plural = "Noticias"
